efdb0323c67f1303c241bfc323c298d1a02dbaf2
Rene-Yanez <rene.yanez2001@alumnos.ubiobio.cl>
2025-06-22 18:50:37 -0400

    caja

---
+++ src/test/java/com/farmaciascomunales/caja/CajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmaciascomunales.caja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
